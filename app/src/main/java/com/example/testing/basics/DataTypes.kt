30f451df0737fb40da519b839fd4d6b4872f3315
Saurabh <saurabh.kumar021@gmail.com>
2020-07-15 21:33:44 +0530

    add data

--- app/src/main/java/com/example/testing/basics/DataTypes.kt
+++ app/src/main/java/com/example/testing/basics/DataTypes.kt
@@ -1,2 +1,19 @@
-package com.example.testing.basics
+import android.content.ContentValues
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.*
 
+
+fun main(args: Array<String>) {
+
+        val list =
+            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val observable =
+            Observable.fromIterable(list)
+        observable.subscribe { item: Int? -> println(item) }
+
+
+}
